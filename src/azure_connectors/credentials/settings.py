16eba6aa20ce22341603f17498ea6c58f1b96d9e
Jerome Hodges <jh4@jainfamilyinstitute.org>
2024-07-06 20:08:22 +0000

    explicit args in AzureSqlSettings.from_env, docs

--- src/azure_connectors/credentials/settings.py
+++ src/azure_connectors/credentials/settings.py
@@ -27,9 +27,14 @@ class AzureCredentialSettings(BaseSettings):
     @classmethod
     def from_env(cls, source: Optional[CredentialSource], scope: Optional[CredentialScope] = None) -> 'AzureCredentialSettings':
         """
-        Create an instance of AzureCredentialSettings by reading the settings not passed explicitly
-          from the environment variables and .env file.
+        Create an instance of AzureCredentialSettings by reading the settings not passed explicitly 
+        from the environment variables and .env file.
+        
         Provided for consistency with dependent classes' from_env methods.
+
+        Args:
+            source (Optional[CredentialSource]): The source of the credentials. If not provided, it will be read from the environment.
+            scope (Optional[CredentialScope]): The scope of the credentials. If not provided, it will be read from the environment.
 
         Returns:
             AzureCredentialSettings: An instance of AzureCredentialSettings with the settings loaded from the environment.
