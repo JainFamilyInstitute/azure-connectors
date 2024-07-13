7eccc0ad687998abbb88b725604dc97cee8a5b6e
Jerome Hodges <jh4@jainfamilyinstitute.org>
2024-07-13 22:22:28 +0000

    update exports

--- src/azure_connectors/__init__.py
+++ src/azure_connectors/__init__.py
@@ -1,2 +1,4 @@
 from .azure_sql import AzureSqlConnection as AzureSqlConnection
+from .azure_sql import SqlManagementClient as SqlManagementClient
 from .azure_tables import TableServiceClient as TableServiceClient
+from .azure_blob import BlobServiceClient as BlobServiceClient
