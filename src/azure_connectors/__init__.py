f1f406b0e4cee8f09c1218d1c28deaac476ca6a7
Jerome Hodges <jh4@jainfamilyinstitute.org>
2024-07-13 22:18:15 +0000

    remove old table connection class

--- src/azure_connectors/__init__.py
+++ src/azure_connectors/__init__.py
@@ -1,2 +1,2 @@
 from .azure_sql import AzureSqlConnection as AzureSqlConnection
-from .azure_tables import AzureTableConnection as AzureTableConnection
+from .azure_tables import TableServiceClient as TableServiceClient
