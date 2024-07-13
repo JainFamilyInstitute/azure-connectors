1ae3d9c00660db81c0ed2eb0ff6580431c9c0c08
Jerome Hodges <jh4@jainfamilyinstitute.org>
2024-07-13 22:10:07 +0000

    refactor client factory to accommodate SQL management client

--- src/azure_connectors/azure_blob/api_clients.py
+++ src/azure_connectors/azure_blob/api_clients.py
@@ -1,8 +1,11 @@
 from azure.storage.blob import BlobServiceClient as AzBlobServiceClient
 
 from azure_connectors.azure_blob.settings import AzureBlobServiceSettings
-from azure_connectors.client_factory import create_azure_client_class
+from azure_connectors.client_factory import ClientFactory
 from azure_connectors.config import CredentialScope
 
-BlobServiceClient = create_azure_client_class(AzBlobServiceClient, settings_class=AzureBlobServiceSettings, scope=CredentialScope.AZURE_BLOB)
-# ContainerClient = create_azure_client_class(AzContainerClient, scope=CredentialScope.AZURE_BLOB)
+BlobServiceClient = ClientFactory(
+    AzBlobServiceClient,
+    settings_class=AzureBlobServiceSettings,
+    scope=CredentialScope.AZURE_BLOB,
+).client
