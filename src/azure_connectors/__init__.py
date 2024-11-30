2241f7d21bce8d247c41d691ce7529cd65a017e4
Mikhael Gaster <mikhaelgaster@gmail.com>
2024-11-30 13:38:18 -0800

    minor: streamline package imports/exports

--- src/azure_connectors/__init__.py
+++ src/azure_connectors/__init__.py
@@ -1,6 +1,18 @@
-from .azure_blob import BlobClient as BlobClient
-from .azure_blob import BlobServiceClient as BlobServiceClient
-from .azure_blob import ContainerClient as ContainerClient
-from .azure_sql import AzureSqlConnection as AzureSqlConnection
-from .azure_sql import SqlManagementClient as SqlManagementClient
-from .azure_tables import TableServiceClient as TableServiceClient
+from .azure_blob import BlobClient
+from .azure_blob import BlobServiceClient
+from .azure_blob import ContainerClient
+from .azure_sql import AzureSqlConnection
+from .azure_sql import SqlManagementClient
+from .azure_tables import TableServiceClient
+from .dataframe_io import read_df, write_df
+
+__all__ = [
+    "BlobClient",
+    "BlobServiceClient",
+    "ContainerClient",
+    "AzureSqlConnection",
+    "SqlManagementClient",
+    "TableServiceClient",
+    "read_df",
+    "write_df",
+]
